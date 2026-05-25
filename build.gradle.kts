b298b01c15adff097d96027b9e1a08ec26b5e470
Wallan <32087983+wallanpsantos@users.noreply.github.com>
2026-05-24 23:29:14 -0300

    feat: UTF-8 na execução Gradle
    Java moderno, especialmente Java 18+, também é útil configurar a saída padrão da JVM

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,11 @@ tasks.withType<Test> {
 
 tasks.withType<JavaExec> {
     systemProperty("file.encoding", "UTF-8")
+    jvmArgs(
+        "-Dfile.encoding=UTF-8",
+        "-Dsun.stdout.encoding=UTF-8",
+        "-Dsun.stderr.encoding=UTF-8"
+    )
 }
 
 repositories {
